826f00c1146b62ac795fd6e791a500f1f293b7c9
Matt <carlotta.matt@gmail.com>
2019-12-24 08:49:21 -0800

    remove styled-components

--- babel.config.js
+++ babel.config.js
@@ -1,43 +1,42 @@
 module.exports = function(api) {
-  api.cache(true);
+	api.cache(true);
 
-  return {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          useBuiltIns: 'entry',
-          corejs: { version: 3, proposals: true },
-        },
-      ],
-      '@babel/preset-react',
-    ],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@actions': './src/actions',
-            '@components': './src/components',
-            '@config': './config',
-            '@containers': './src/containers',
-            '@images': './src/images',
-            '@pages': './src/pages',
-            '@reducers': './src/reducers',
-            '@root': './src/root',
-            '@routes': './src/routes',
-            '@styles': './src/styles',
-            '@types': './src/types',
-            '@utils': './src/utils',
-          },
-        },
-      ],
-      '@babel/plugin-transform-runtime',
-      'babel-plugin-styled-components',
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      '@babel/plugin-syntax-dynamic-import',
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      'lodash',
-    ],
-  };
+	return {
+		presets: [
+			[
+				"@babel/preset-env",
+				{
+					useBuiltIns: "entry",
+					corejs: { version: 3, proposals: true },
+				},
+			],
+			"@babel/preset-react",
+		],
+		plugins: [
+			[
+				"module-resolver",
+				{
+					alias: {
+						"@actions": "./src/actions",
+						"@components": "./src/components",
+						"@config": "./config",
+						"@containers": "./src/containers",
+						"@images": "./src/images",
+						"@pages": "./src/pages",
+						"@reducers": "./src/reducers",
+						"@root": "./src/root",
+						"@routes": "./src/routes",
+						"@styles": "./src/styles",
+						"@types": "./src/types",
+						"@utils": "./src/utils",
+					},
+				},
+			],
+			"@babel/plugin-transform-runtime",
+			["@babel/plugin-proposal-decorators", { legacy: true }],
+			"@babel/plugin-syntax-dynamic-import",
+			["@babel/plugin-proposal-class-properties", { loose: true }],
+			"lodash",
+		],
+	};
 };
